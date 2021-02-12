643ea8ca3d87b308a7ed9e5247ffa0205ed53185
Jhon Achata Limachi <jhonal183@gmail.com>
2021-02-12 17:21:31 -0500

    setup firebase config with dotenv

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './routes'
-import firebase from 'firebase/app'
-import 'firebase/database'
+import App from './App.vue'
+
 Vue.config.productionTip = false
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDYULGfjGoXbVsai4kCMpAq-KfA41uIhaU',
-  authDomain: 'team-vuejs5-comfeco.firebaseapp.com',
-  projectId: 'team-vuejs5-comfeco',
-  storageBucket: 'team-vuejs5-comfeco.appspot.com',
-  messagingSenderId: '385421612063',
-  appId: '1:385421612063:web:4637e1de223d911832b12a',
-  measurementId: 'G-X3DPNPGT1X'
-}
-firebase.initializeApp(firebaseConfig)
 new Vue({
   router,
   render: h => h(App)
