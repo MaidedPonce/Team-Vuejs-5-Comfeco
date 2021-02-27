da67e1b3d76bae90ed22ad273e1851b30e3f16a2
Maided <maided.h.p90@gmail.com>
2021-02-26 22:04:17 -0600

    counter update

--- src/views/auth/Private.vue
+++ src/views/auth/Private.vue
@@ -3,6 +3,15 @@
 		<button @click="logout()" to="/login" class="bg-red-400 block max-w-min text-sm px-4 py-2 leading-none border rounded text-white border-red-800 mt-4 md:mt-0 md:inline-block md:mx-1 lg:mt-0">Logout</button>
 		<Carousel />
 		<CarouselMini />
+		<Counter 
+			:year="2021"
+			:month="1"
+			:date="27"
+			:hour="23"
+			:minute="59"
+			:second="59"
+			:millisecond="31"
+	/>
 	</div>
 </template>
 
@@ -11,12 +20,13 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import Carousel from '../../components/Carousel'
 import CarouselMini from '../../components/CarouselMini'
-
+import Counter from '../../components/Counter'
 export default {
     name: 'Private',
 	components: {
 		Carousel,
 		CarouselMini,
+		Counter
 	},
     methods: {
 		logout() {
