9f482ef7ea8e168ac37a129a6df3c36302ed8c55
Vasquez Gutierrez Jose Eduardo <pepvaslmd@gmail.com>
2021-03-18 15:48:21 -0600

    update badge

--- src/views/private/profile/Badges.vue
+++ src/views/private/profile/Badges.vue
@@ -1,11 +1,42 @@
 <template>
   <section>
-    Badges
+    <p v-if="badges.sociable"> Insignia sociable obtenia </p>
   </section>
 </template>
 
 <script>
-export default {};
+
+import { auth, db } from '../../../config/firebase';
+
+export default {
+  name: 'Badge',
+
+  data(){
+    return{
+      badges: {
+        sociable: false
+      }
+    }
+  },
+
+  mounted(){
+    this.checkBadgeSociable()
+  },
+
+  methods: {
+    checkBadgeSociable: async function(){
+      const authUser = auth.currentUser;
+
+      const document = await db
+        .collection('users/' + authUser.uid + '/badges')
+        .doc('Sociable')
+        .get();
+        
+      let badgeSociable = document.data();
+      this.badges.sociable = badgeSociable.aviable;
+    }
+  }
+};
 </script>
 
 <style></style>
