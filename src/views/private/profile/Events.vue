650b887da1644bb1ecb0672d3a6773ef8a520038
Jhon Achata Limachi <jhonal183@gmail.com>
2021-03-20 13:47:43 -0500

    fix events align

--- src/views/private/profile/Events.vue
+++ src/views/private/profile/Events.vue
@@ -2,16 +2,25 @@
   <section
     class="w-11/12 md:10/12 m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 pt-16"
   >
-
-    <div class="max-w-xs overflow-hidden rounded-lg shadow-lg" v-for="(event, index) in events" :key="index"> 
-      <img class="object-cover w-full h-48" src="../../../assets/images/time.jpg" alt="Flower and sky"/>
+    <div
+      class="max-w-xs m-auto overflow-hidden rounded-lg shadow-lg"
+      v-for="(event, index) in events"
+      :key="index"
+    >
+      <img
+        class="object-cover w-full h-48"
+        src="../../../assets/images/time.jpg"
+        alt="Flower and sky"
+      />
       <div class="px-6 py-4">
-        <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">{{event.name}}</h4>
-        <p class="leading-normal text-gray-700">{{event.description}}</p>
+        <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">
+          {{ event.name }}
+        </h4>
+        <p class="leading-normal text-gray-700">{{ event.description }}</p>
       </div>
 
       <button
-        class="h-10 px-5 m-2 text-green-100 transition-colors duration-150 bg-green-700 rounded-lg focus:shadow-outline hover:bg-green-800"
+        class="px-6 m-2 text-green-100 transition-colors duration-150 bg-green-700 rounded-lg focus:shadow-outline hover:bg-green-800 py-2"
         @click="joinEvent(event.name)"
         :disabled="user.event !== ''"
         v-if="event.name !== user.event"
@@ -20,7 +29,9 @@
       </button>
 
       <section v-else>
-        <div class="px-4 py-3 leading-normal text-blue-700 bg-blue-100 rounded-lg">
+        <div
+          class="px-4 py-3 leading-normal text-blue-700 bg-blue-100 rounded-lg"
+        >
           <p>Ya estas participando</p>
         </div>
 
@@ -31,7 +42,6 @@
           Abandonar
         </button>
       </section>
-      
     </div>
   </section>
 </template>
@@ -39,66 +49,68 @@
 <script>
 import { auth, db } from '../../../config/firebase';
 export default {
-  data(){
-    return{
+  data() {
+    return {
       events: [],
       user: {
-        event: ''
-      }
-    }
+        event: '',
+      },
+    };
   },
 
-  mounted(){
-    this.getEvents()
-    this.getUser()
+  mounted() {
+    this.getEvents();
+    this.getUser();
   },
 
   methods: {
-    getUser: async function(){
+    getUser: async function() {
       const authUser = auth.currentUser;
 
-      let document = await db.collection('users').doc(authUser.uid).get()
+      let document = await db
+        .collection('users')
+        .doc(authUser.uid)
+        .get();
       let user = document.data();
 
-      this.user.event = user.event
-
+      this.user.event = user.event;
     },
 
-    getEvents: async function(){
-      const events = await db
-        .collection('events')
-        .get();
+    getEvents: async function() {
+      const events = await db.collection('events').get();
 
-      events.forEach(doc => {
+      events.forEach((doc) => {
         let data = doc.data();
-        let new_event = {name: doc.id, description: data.description}
-        this.events.push(new_event)
+        let new_event = { name: doc.id, description: data.description };
+        this.events.push(new_event);
       });
     },
 
-    joinEvent: async function(name){
-      const authUser = auth.currentUser
+    joinEvent: async function(name) {
+      const authUser = auth.currentUser;
 
-      await db.collection('users')
+      await db
+        .collection('users')
         .doc(authUser.uid)
         .update({
-          event: name
+          event: name,
         });
 
-      this.user.event = name
+      this.user.event = name;
     },
 
-    leaveEvent: async function(){
-      const authUser = auth.currentUser
+    leaveEvent: async function() {
+      const authUser = auth.currentUser;
 
-      await db.collection('users')
+      await db
+        .collection('users')
         .doc(authUser.uid)
         .update({
-          event: ''
+          event: '',
         });
 
-      this.user.event = ''
-    }
+      this.user.event = '';
+    },
   },
 };
 </script>
--- src/views/private/profile/Groups.vue
+++ src/views/private/profile/Groups.vue
@@ -1,19 +1,24 @@
 <template>
   <section
-  class="w-11/12 md:w-8/12 lg:w-11/12 m-auto flex-grow py-16 grid grid-cols-1 lg:grid-cols-5 lg:gap-4 lg:gap-x-12">
+    class="w-11/12 md:w-8/12 lg:w-11/12 m-auto flex-grow py-16 grid grid-cols-1 lg:grid-cols-5 lg:gap-4 lg:gap-x-12"
+  >
     <div class="p-4 col-span-1">
       <section class="space-y-4">
         <div class="flex justify-between">
-          <h3 class="text-xl font-semibold">{{getGroupName}}</h3>
+          <h3 class="text-xl font-semibold">{{ getGroupName }}</h3>
           <div class="text-right">
-        <a href="#" class="text-sm text-blue-500 font-semibold">
-          Ir al grupo
-        </a>
-      </div>
+            <a href="#" class="text-sm text-blue-500 font-semibold">
+              Ir al grupo
+            </a>
+          </div>
         </div>
       </section>
 
-      <div class="flex py-3 mt-4 space-x-2 border-b border-purple-500" v-for="index in [1, 2, 3, 4, 5]" :key="index">
+      <div
+        class="flex py-3 mt-4 space-x-2 border-b border-purple-500"
+        v-for="index in [1, 2, 3, 4, 5]"
+        :key="index"
+      >
         <figure class="w-12 h-12 rounded-full overflow-hidden">
           <img src="../../../assets/images/avatar.jpg" alt="avatar" />
         </figure>
@@ -31,51 +36,72 @@
     </div>
 
     <div class="col-span-4">
-        <div class="flex flex-wrap -mx-2 space-y-4 md:space-y-0">
-          <div class="w-full px-2 md:w-1/2">
-            <select class="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" v-model="filterByLanguage">
-                <option value="">Filtrar por lenguaje</option>
-                <option>C++</option>
-                <option>Haskell</option>
-                <option>Typescript</option>
-                <option>PHP</option>
-                <option>Python</option>
-                <option>Java</option>
-                <option>Javascript</option>
-                <option>C#</option>
-            </select>
-          </div>
-          <div class="w-full px-2 md:w-1/2">
-            <input class="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" placeholder="Buscar" type="text" 
-            v-model="searchByName"
-            v-on:keyup.enter="$event.target.blur()"/>
-          </div>
+      <div class="flex flex-wrap -mx-2 space-y-4 md:space-y-0">
+        <div class="w-full px-2 md:w-1/2">
+          <select
+            class="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+            v-model="filterByLanguage"
+          >
+            <option value="">Filtrar por lenguaje</option>
+            <option>C++</option>
+            <option>Haskell</option>
+            <option>Typescript</option>
+            <option>PHP</option>
+            <option>Python</option>
+            <option>Java</option>
+            <option>Javascript</option>
+            <option>C#</option>
+          </select>
         </div>
+        <div class="w-full px-2 md:w-1/2">
+          <input
+            class="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+            placeholder="Buscar"
+            type="text"
+            v-model="searchByName"
+            v-on:keyup.enter="$event.target.blur()"
+          />
+        </div>
+      </div>
 
-
-    <div class="container mx-auto">
-      <div class="flex flex-wrap -mx-4">
-      
-        <div class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4"  v-for="(group, index) in getGroupsFilter" :key="index">
-          <div class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-            <div class="relative pb-48 overflow-hidden">
-              <img class="absolute inset-0 h-full w-full object-cover" src="../../../assets/images/card_group.jpg" alt="">
-            </div>
-            <div class="p-4">
-              <span class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">{{group.language}}</span>
-              <h2 class="mt-2 mb-2  font-bold">{{group.name}}</h2>
-              <p class="text-sm">{{group.description}}</p>
-              <div class="mt-3 flex items-center justify-center">
-                <button class="h-10 px-5 m-2 text-blue-100 transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700"
-                 @click="joinGroup(group.name)">Unirse</button>
+      <div class="container mx-auto">
+        <div class="flex flex-wrap -mx-4">
+          <div
+            class="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4"
+            v-for="(group, index) in getGroupsFilter"
+            :key="index"
+          >
+            <div
+              class="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden"
+            >
+              <div class="relative pb-48 overflow-hidden">
+                <img
+                  class="absolute inset-0 h-full w-full object-cover"
+                  src="../../../assets/images/card_group.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="p-4">
+                <span
+                  class="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs"
+                  >{{ group.language }}</span
+                >
+                <h2 class="mt-2 mb-2  font-bold">{{ group.name }}</h2>
+                <p class="text-sm">{{ group.description }}</p>
+                <div class="mt-3 flex items-center justify-center">
+                  <button
+                    class="h-10 px-5 m-2 text-blue-100 transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700"
+                    @click="joinGroup(group.name)"
+                  >
+                    Unirse
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-    
   </section>
 </template>
 
@@ -85,85 +111,93 @@ import { auth, db } from '../../../config/firebase';
 export default {
   name: 'Badge',
 
-  data(){
-    return{
+  data() {
+    return {
       groups: [],
       filterByLanguage: '',
       searchByName: '',
       user: {
-        group: ''
-      }
-    }
+        group: '',
+      },
+    };
   },
 
-  mounted(){
+  mounted() {
     this.getGroups();
     this.getUser();
   },
 
   computed: {
-    getGroupsFilter(){
-      return this.getGroupsFilterByName.filter(x => this.getGroupsFilterByLanguage.includes(x))
+    getGroupsFilter() {
+      return this.getGroupsFilterByName.filter((x) =>
+        this.getGroupsFilterByLanguage.includes(x)
+      );
     },
 
-    getGroupsFilterByLanguage(){
-      if(this.filterByLanguage === '') return this.groups;
+    getGroupsFilterByLanguage() {
+      if (this.filterByLanguage === '') return this.groups;
 
-      return this.groups.filter((val) =>{
-        return val.language.toLowerCase() === this.filterByLanguage.toLowerCase()
-      })
-    },
-
-    getGroupsFilterByName(){
-      if(this.searchByName === '') return this.groups;
-
-      return this.groups.filter((val) =>{
-        return val.name.toLowerCase().includes(this.searchByName.toLowerCase())
-      })
-    },
-
-    getGroupName(){
-      if(this.user.group === undefined)
-        return 'Sin grupo'
-
-      return this.user.group
-    }
-  },
-
-  methods: {
-    getGroups: async function(){
-      const groups = await db
-        .collection('groups')
-        .get();
-
-      groups.forEach(doc => {
-        let data = doc.data();
-        let new_group = {name: doc.id, description: data.description, language: data.language }
-        this.groups.push(new_group)
+      return this.groups.filter((val) => {
+        return (
+          val.language.toLowerCase() === this.filterByLanguage.toLowerCase()
+        );
       });
     },
 
-    getUser: async function(){
-      const authUser = auth.currentUser;
+    getGroupsFilterByName() {
+      if (this.searchByName === '') return this.groups;
 
-      let document = await db.collection('users').doc(authUser.uid).get()
-      let user = document.data();
-
-      this.user.group = user.group
-
+      return this.groups.filter((val) => {
+        return val.name.toLowerCase().includes(this.searchByName.toLowerCase());
+      });
     },
 
-    joinGroup: async function(name){
-      const authUser = auth.currentUser
+    getGroupName() {
+      if (this.user.group === undefined) return 'Mi Grupo';
 
-      await db.collection('users')
+      return this.user.group;
+    },
+  },
+
+  methods: {
+    getGroups: async function() {
+      const groups = await db.collection('groups').get();
+
+      groups.forEach((doc) => {
+        let data = doc.data();
+        let new_group = {
+          name: doc.id,
+          description: data.description,
+          language: data.language,
+        };
+        this.groups.push(new_group);
+      });
+    },
+
+    getUser: async function() {
+      const authUser = auth.currentUser;
+
+      let document = await db
+        .collection('users')
+        .doc(authUser.uid)
+        .get();
+      let user = document.data();
+
+      this.user.group = user.group;
+    },
+
+    joinGroup: async function(name) {
+      const authUser = auth.currentUser;
+
+      await db
+        .collection('users')
         .doc(authUser.uid)
         .update({
-          group: name
+          group: name,
         });
 
-      this.user.group = name
-    }
+      this.user.group = name;
+    },
   },
 };
 </script>
